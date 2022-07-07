f11d7992621d5b63a902292bf278196eec7924f2
ajeet-ydv <yadavajeet1553@gmail.com>
2022-07-07 23:09:20 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Header from "./components/Header";
 import Recipes from "./components/Recipes";
 import Axios from "axios";
+import Loader from "./components/Loader";
 
 function App() {
-  const [search, setSerach] = useState("chiken");
+  const [search, setSerach] = useState("paneer");
   const [recipes, setRecipes] = useState([]);
+  const [isSpinning, setIsSpinning] = useState(false);
 
   const APP_ID = "d57e55e2";
   const APP_KEY = "3a59385402d7cd19239e62d958a3fb7e";
@@ -16,14 +18,19 @@ function App() {
   }, []);
 
   const getRecipes = async () => {
-    const res = await Axios.get(
-      `https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
-    console.log()
-    setRecipes(res.data.hits);
+    try {
+      setIsSpinning(true);
+      const res = await Axios.get(
+        `https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+      setRecipes(res.data.hits);
+      setIsSpinning(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const onInputChange = e => {
+  const onInputChange = (e) => {
     setSerach(e.target.value);
   };
 
@@ -31,16 +38,22 @@ function App() {
     getRecipes();
   };
   return (
-    <div className="App">
-      <Header
-        search={search}
-        onInputChange={onInputChange}
-        onSearchClick={onSearchClick}
-      />
-      <div className="container">
-        <Recipes recipes={recipes} />
-      </div>
-    </div>
+    <>
+      {isSpinning ? (
+        <Loader />
+      ) : (
+        <div className="App">
+          <Header
+            search={search}
+            onInputChange={onInputChange}
+            onSearchClick={onSearchClick}
+          />
+          <div className="container">
+            <Recipes recipes={recipes} />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div id="loader-wrap">
+     <div class="loader">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    </div>
+  );
+};
+
+export default Loader;
